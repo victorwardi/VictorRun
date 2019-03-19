550e36d88234cd8bc10f14a0f22b11e8458f7cd7
Victor Wardi <victorcaetano@live.com>
2019-03-19 14:27:18 -0300

    terminal css and home page.

--- src/app/ui/pages/terminal/terminal.component.ts
+++ src/app/ui/pages/terminal/terminal.component.ts
@@ -12,12 +12,17 @@ export class TerminalComponent {
     terminalLines = new Array();
 
     @ViewChild('customScrollbar') private customScrollbar: ElementRef;
+    @ViewChild('terminalInput') private terminalInput: ElementRef;
 
     constructor() {
     }
 
+    onMouseEnterTerminal(){
+        console.log('mouse over terminal');
+        this.terminalInput.nativeElement.focus();
+    }
 
-    onEnter(command: string) {
+    onUserClickEnter(command: string) {
 
         const commandToLowerCase = command.toLowerCase();
 
@@ -30,63 +35,74 @@ export class TerminalComponent {
             case 'cls' :
                 this.terminalLines = new Array();
                 break;
-            // Show Available Commands
+            // Show Available Command
             case '0':
             case 'h':
             case 'help':
+                this.getCommand(command);
                 this.getHelp();
                 break;
             // All Work Experiences
             case '1':
             case 'w':
             case 'work':
+                this.getCommand(command);
                 this.getWork(command, 'all');
                 break;
             // Correios
             case 'wc':
             case 'correios':
+                this.getCommand(command);
                 this.getWork(command, 'correios');
                 break;
             // Petrobras
             case 'wp':
-            case 'petrobas':
+            case 'petrobras':
+                this.getCommand(command);
                 this.getWork(command, 'petrobras');
                 break;
             // Old Companies
             case 'wo':
             case 'old':
                 break;
+                this.getCommand(command);
                 this.getWork(command, 'petrobras');
                 break;
             // Description
             case '2':
             case 'd':
             case 'description':
+                this.getCommand(command);
                 break;
             // Shows all Studies
             case '3':
             case 's':
             case 'study':
+                this.getCommand(command);
                 break;
             //  Shows all hobbies.
             case '4':
             case 'h':
             case 'hobby':
+                this.getCommand(command);
                 break;
             // Shows all certifications.
             case '5':
             case 'c':
             case 'certification':
+                this.getCommand(command);
                 break;
             // Export a PDF version of the resume.
             case '6':
             case 'e':
             case 'export':
+                this.getCommand(command);
                 break;
             // Contact.
             case '7':
             case 'cc':
             case 'contact':
+                this.getCommand(command);
                 this.getContacts(command);
                 break;
             // Command is not recognized.
@@ -102,10 +118,11 @@ export class TerminalComponent {
 
 
     getCommand(command: string) {
-        this.terminalLines.push('<p class="terminal-command">' + command + '</p>');
+        this.terminalLines.push('<p class="terminal-command">[user@victor.run] <i class="fas fa-terminal vr-terminal-icon"></i> ' + command + '</p>');
     }
 
     getHelp() {
+
 
         let help = '';
 
@@ -129,7 +146,7 @@ export class TerminalComponent {
 
         if (company === 'all' || company === 'correios') {
             work += '<p class="company-title mt-2" >' +
-                '<img src="./assets/images/correios.png" height="15px" alt="Correios">  Correios (Brazilian Post)</p>';
+                '<img src="./assets/images/correios.png" height="15px" alt="Correios"> Correios (Brazilian Post)</p>';
             work += '<p class="company-work-period>From Aug 2011 to Current Job</p>';
             work += '<p class="company-role">Role: Software Developer</p>';
             work += '<p class="company-description">' +
@@ -146,24 +163,27 @@ export class TerminalComponent {
             work += '<li>Versioned with Subversion SVN, built and delivered automatically with Maven and Jenkins.</li>';
             work += '<li>Refactored following Clean Code and SOLID principles, making the code more legible and easy to maintain.</li>';
             work += '</ul>';
-            work += '<p class="">/p>';
+
 
 
         }
 
         if (company === 'all' || company === 'petrobras') {
-            work += '<p class="company-title mt-2"><img src="./assets/images/petrobras.png" height="15px" alt="Petrobras">Petrobras</p>';
-            work += '<p class="terminal-command2">  > Petrobras - 2009 - 2011</p>';
-            work += '<p class="terminal-command2">      * Java Web Developer</p>';
+            work += '<p class="company-title mt-2" >' +
+                '<img src="./assets/images/petrobras.png" height="15px" alt="Petrobras"> Petrobras (Outsourced)</p>';
+            work += '<p class="company-work-period>From Aug 2011 to Current Job</p>';
+            work += '<p class="company-role">Role: Java Software Developer</p>';
+            work += '<p class="company-description">' +
+                'One of the largest companies in the country with more than 100,000 employees. ' +
+                'The Brazilian Post is a state-owned company that operates the national postal service of Brazil.</p>';
+
+            work += '<p class="company-projects">Main Projects:</p>';
+            work += '<p class="">Electronic Invoice System</p>';
+            work += '<p class="">System responsible for issuing a copy of the invoice (bank slip) and the invoice statement,' +
+                ' through the Internet, for more than 20,000 clients.</p>';
             work += '<ul>';
             work += '<li>Developed using Java EE, EJB3, JSF 2 (Primefaces), Facelets, CDI, JPA, Hibernate and Web Services.</li>';
-            work += '<li>Reports created with Jasper.';
-            work += '<ul>';
-            work += '<li>Total 1</li>';
-            work += '<li>Total 2</li>';
-            work += '<li>Total 3</li>';
-            work += '</ul>';
-            work += '</li>';
+            work += '<li>Reports created with Jasper.</li>';
             work += '<li>Versioned with Subversion SVN, built and delivered automatically with Maven and Jenkins.</li>';
             work += '<li>Refactored following Clean Code and SOLID principles, making the code more legible and easy to maintain.</li>';
             work += '</ul>';

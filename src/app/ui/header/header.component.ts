547a2efbc18f29ddb5a03682fac8b15f4970b530
Victor Wardi <victorcaetano@live.com>
2019-04-08 21:09:15 -0300

    Mobile Menu

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    showMenu = false;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
+
+    toogleMenu() {
+        this.showMenu = !this.showMenu;
+    }
 
 }

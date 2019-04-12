332f3f323ba52a132e1d429718c3b380b8a616a4
Victor Wardi <victorcaetano@live.com>
2019-04-12 11:03:13 -0300

    CSS Menu Transition

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,14 +1,33 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
     encapsulation: ViewEncapsulation.None,
+    animations: [
+        trigger('collapse', [
+            state('open', style({
+                opacity: '1',
+                display: 'block',
+                height: '100%',
+                //transform: 'translate3d(0, 0, 0)'
+            })),
+            state('closed',   style({
+                opacity: '0',
+                display: 'none',
+                height: '0%',
+              //  transform: 'translate3d(0, -100%, 0)'
+            })),
+            transition('closed => open', animate('500ms ease-in')),
+            transition('open => closed', animate('100ms ease-out'))
+        ])
+    ]
 })
 export class HeaderComponent implements OnInit {
 
-    showMenu = false;
+    collapse:string = "closed";
 
     constructor() {
     }
@@ -16,8 +35,8 @@ export class HeaderComponent implements OnInit {
     ngOnInit() {
     }
 
-    toogleMenu() {
-        this.showMenu = !this.showMenu;
+    toogleCollapse() {
+       this.collapse = this.collapse == 'open' ? 'closed' : 'open';
     }
 
 }
